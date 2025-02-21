74cee5f8070ea4b9810d08ffbaa890e07d3ebebb
Wilson <wilson917@gmail.com>
2025-02-21 00:46:56 -0700

    pass APP_NAME variable

--- SpringBoot/src/main/java/com/docker/demo/HomeController.java
+++ SpringBoot/src/main/java/com/docker/demo/HomeController.java
@@ -3,12 +3,19 @@ package com.docker.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Value;
+
 
 @RestController
 public class HomeController {
+
+    @Value("${APP_NAME:Default App}")
+    private String appName;
+
+
     @GetMapping("/")
     public String home() {
-        return "Docker A"; // For Service B, change the returned string to "Docker B"
+        return appName;
     }
 }
 
